bd23a5a991805c08b4c120c8ee5b45626421d4a6
Hongkai Liu <115027495+hongkailiu123@users.noreply.github.com>
2026-04-23 19:35:32 -0700

    Add: time-based-key-value-store - submission-15

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-15.py
@@ -0,0 +1,31 @@
+class TimeMap:
+
+    def __init__(self):
+        #key, list of value + timestamp
+        self.store = {}
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.store.keys():
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        if key not in self.store.keys():
+            return ""
+        keyList = self.store[key]
+        # the values were stored in ascending order of timestamp
+        left = 0
+        right = len(keyList) - 1
+        ans = ""
+        
+        while left <= right:
+            mid = (left + right)//2
+            if keyList[mid][1] <= timestamp:
+                ans = keyList[mid][0]
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return ans
+
+
